da7597e911b0d1fd4208007302a2d0e2001774a7
Toni Lazaro <anlazes@gmail.com>
2013-12-29 21:21:24 +0100

    Añadido de variables y cambio de mensaje impreso

--- PruebaGit/src/PrimerRepo.java
+++ PruebaGit/src/PrimerRepo.java
@@ -3,9 +3,12 @@ public class PrimerRepo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = "Hola a todos!";
+		int x=8;
+		int y=7;
+		String a = "los numeros son: ";
 		
-		System.out.println(a);
+		
+		System.out.println(a+x+" y "+y);
 
 	}
 
