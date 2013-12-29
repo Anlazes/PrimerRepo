d6c8c1237a57ff3ec4c0400a3c1290dec89f683f
Toni Lazaro <anlazes@gmail.com>
2013-12-29 18:33:53 +0100

    Cambio del nombre de la variable.

--- PruebaGit/src/PrimerRepo.java
+++ PruebaGit/src/PrimerRepo.java
@@ -3,9 +3,9 @@ public class PrimerRepo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String word = "Hola a todos!";
+		String a = "Hola a todos!";
 		
-		System.out.println(word);
+		System.out.println(a);
 
 	}
 
