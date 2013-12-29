21fe6905f426b173a824205c846c522a98218bb7
Toni Lazaro <anlazes@gmail.com>
2013-12-29 18:26:33 +0100

    Primer cambio. Comienzo del proyecto.

---
+++ PruebaGit/src/PrimerRepo.java
@@ -0,0 +1,12 @@
+
+public class PrimerRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word = "Hola a todos!";
+		
+		System.out.println(word);
+
+	}
+
+}
